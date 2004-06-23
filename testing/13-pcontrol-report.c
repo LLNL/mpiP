f5519dcbe4d4e9316526d11da58c3039fdffda82
chcham <chcham@b8bbd010-f11e-0410-ac07-ecf92694ee1a>
2004-06-23 17:12:33 +0000

    Test for report generation through pcontrol
    git-svn-id: http://svn.code.sf.net/p/mpip/code/trunk@102 b8bbd010-f11e-0410-ac07-ecf92694ee1a

---
+++ testing/13-pcontrol-report.c
@@ -0,0 +1,43 @@
+/* 13-pcontrol-report.c  -- test report generation with MPI_Pcontrol(-1) */
+/* Chris Chambreau (chcham@llnl.gov)  */
+/* LLNL */
+
+#ifndef lint
+static char *rcsid =
+  "$Header$";
+#endif
+
+#include <stdio.h>
+#include <unistd.h>
+#include "mpi.h"
+
+main (int argc, char **argv)
+{
+  int nprocs = -1;
+  int rank = -1;
+  char buf[256];
+
+  MPI_Init (&argc, &argv);	/* use cmd line to disable mpip initially */
+  MPI_Comm_size (MPI_COMM_WORLD, &nprocs);
+  MPI_Comm_rank (MPI_COMM_WORLD, &rank);
+  gethostname (buf, 256);
+  printf ("MPI comm size is %d with rank %d executing on %s\n",
+	  nprocs, rank, buf);
+
+  MPI_Barrier (MPI_COMM_WORLD);
+  sleep(1);
+  MPI_Barrier (MPI_COMM_WORLD);
+  MPI_Pcontrol (-1);		/* generate report */
+  MPI_Barrier (MPI_COMM_WORLD);
+  exit(1);
+#if 0
+  sleep(1);
+  MPI_Barrier (MPI_COMM_WORLD);
+  MPI_Pcontrol (-1);		/* generate report */
+  sleep(1);
+  MPI_Barrier (MPI_COMM_WORLD);
+  sleep(1);
+  MPI_Finalize ();
+#endif
+}
+				 /* EOF */
