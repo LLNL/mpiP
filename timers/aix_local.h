8f72a9c8ba6e51428ceb8f818097f68a799562c4
Chris Chambreau <cchambreau@users.noreply.github.com>
2004-07-14 16:32:20 +0000

    *** empty log message ***

--- timers/aix_local.h
+++ timers/aix_local.h
@@ -21,32 +21,57 @@
 #define mpiPi_TIME timebasestruct_t
 #define mpiPi_ASNTIME(lhs,rhs) {bcopy(rhs, lhs, sizeof(mpiPi_TIME));}
 #define mpiPi_GETTIME(timer,timeaddr) {read_real_time(timeaddr,TIMEBASE_SZ);}
-# if 0
-#define mpiPi_GETUSECS(timer,timeaddr) (_aix_cnvt_read_real_time (timeaddr))
-static long
-_aix_cnvt_read_real_time (mpiPi_TIME * t)
-{
-  int secs, nsecs;
-  time_base_to_time (t, TIMEBASE_SZ);
-  secs = t->tb_high;
-  nsecs = t->tb_low;
-  return /* usecs */ (secs * USECS) + (nsecs / 1000);
-}
-# endif
-static long
+
+extern void mpiPi_msg_warn (char *fmt, ...);
+
+static double
 _aix_cnvt_read_real_time_diff (mpiPi_TIME * end, mpiPi_TIME * start)
 {
   int secs, nsecs;
+  double delta_usecs;
+
+#if defined(MPIP_CHECK_TIME)
+  mpiPi_TIME save_start, save_end;
+
+  if ( 1 )
+  {
+    save_start.tb_high = start->tb_high;
+    save_start.tb_low  = start->tb_low;
+    save_end.tb_high = end->tb_high;
+    save_end.tb_low  = end->tb_low;
+  }
+#endif
+
   time_base_to_time (start, TIMEBASE_SZ);
   time_base_to_time (end, TIMEBASE_SZ);
-  secs = end->tb_high - start->tb_high;
+
+  secs  = end->tb_high - start->tb_high;
   nsecs = end->tb_low - start->tb_low;
+
   if (nsecs < 0)
     {
       secs--;
       nsecs += 1000000000;
     }
-  return /* usecs */ (secs * USECS) + (nsecs / 1000);
+
+  delta_usecs = (double)((secs * USECS) + (nsecs / 1000));
+
+#if defined(MPIP_CHECK_TIME)
+
+  if ( delta_usecs < 0 )
+    {
+      mpiPi_msg_warn ("Found negative time value.\n");
+      mpiPi_msg_warn ("       delta_usecs is %f\n", delta_usecs);
+      mpiPi_msg_warn ("             nsecs is %d\n", nsecs);
+      mpiPi_msg_warn ("              secs is %d\n",  secs);
+      mpiPi_msg_warn ("save_start.tb_high is %u\n", save_start.tb_high);
+      mpiPi_msg_warn (" save_start.tb_low is %u\n", save_start.tb_low);
+      mpiPi_msg_warn ("  save_end.tb_high is %u\n", save_end.tb_high);
+      mpiPi_msg_warn ("   save_end.tb_low is %u\n", save_end.tb_low);
+    }
+#endif
+
+  return delta_usecs;
 }
 
 #define mpiPi_GETTIMEDIFF(timer,end,start) _aix_cnvt_read_real_time_diff(end,start)
