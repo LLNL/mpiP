aba27fd745030315230f3ddb7267d142cd3b74df
chcham <chcham@b8bbd010-f11e-0410-ac07-ecf92694ee1a>
2004-07-20 21:45:19 +0000

    Correct printing of time difference macro
    git-svn-id: http://svn.code.sf.net/p/mpip/code/trunk@137 b8bbd010-f11e-0410-ac07-ecf92694ee1a

--- _timers.h
+++ _timers.h
@@ -36,7 +36,7 @@
 #define mpiPi_GETUSECS(timer,timeaddr) ((((timeaddr)->tv_sec*USECS)+(timeaddr)->tv_usec))
 #define mpiPi_PRINTTIME(taddr) printf("Time is %ld sec and %ld usec.\n", (taddr)->tv_sec, (taddr)->tv_usec)
 #define mpiPi_GETTIMEDIFF(timer,end,start) ((((end)->tv_sec*USECS)+(end)->tv_usec)-(((start)->tv_sec*USECS)+(start)->tv_usec))
-#define mpiPi_PRINTTIMEDIFF(timer,end,start) {printf("Time diff is %ld usecs.\n",mpiPi_GETTIMEDIFF(end,start));}
+#define mpiPi_PRINTTIMEDIFF(timer,end,start) {printf("Time diff is %ld usecs.\n",mpiPi_GETTIMEDIFF(timer,end,start));}
 
 #endif
 
