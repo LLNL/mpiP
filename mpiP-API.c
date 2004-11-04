b058e9575579623fced5a079c12a8db19ee43bc4
chcham <chcham@b8bbd010-f11e-0410-ac07-ecf92694ee1a>
2004-11-04 23:18:31 +0000

    removed unnecessary version of function mpiP_open_executable
    git-svn-id: http://svn.code.sf.net/p/mpip/code/trunk@164 b8bbd010-f11e-0410-ac07-ecf92694ee1a

--- mpiP-API.c
+++ mpiP-API.c
@@ -53,7 +53,7 @@ int mpiP_record_traceback(void* pc_array[], int max_stack)
   return mpiPi_RecordTraceBack(jb, pc_array, max_stack, 1);
 }
 
-int
+extern int
 mpiP_find_src_loc(void *i_addr_hex, char **o_file_str, int *o_lineno,
               char **o_funct_str);
 
@@ -79,43 +79,6 @@ int mpiP_open_executable(char* filename)
 
   return 0;
 }
-
-#ifdef IS_THIS_NECESSARY
-int mpiP_open_executable(char* filename)
-{
-  char* cwd = NULL;
-  char* workname = NULL;
-  int len = 0;
-
-  if ( mpiP_api_init == 0 )
-    mpiP_init_api();
-
-  if ( access(filename, R_OK|F_OK) != 0 )
-  	return -1;
-
-  /*  open_bfd_executable will fail without full path  */
-  if ( filename[0] != '/' )
-  {
-    cwd = getcwd(NULL, 0);
-    len = strlen(cwd) + strlen(filename);
-    workname = malloc(len+1);
-    if ( workname == NULL )
-    	return -1;
-    strcpy(workname, cwd);
-    strcat(workname, filename);
-  }
-
-  open_bfd_executable(filename);
-
-  if ( cwd != NULL )
-  	free(cwd);
-
-  if ( workname != NULL )
-  	free(workname);
-
-  return 0;
-}
-#endif
 
 
 void mpiP_close_executable()
