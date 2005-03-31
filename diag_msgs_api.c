f9fe77e5cc97b9e6d3f432a7bdd8878591ffaffb
chcham <chcham@b8bbd010-f11e-0410-ac07-ecf92694ee1a>
2005-03-31 23:49:39 +0000

    Added diag_msgs_api.c and Makefile.in target libmpiP-API.a to make an API target library that does not profile MPI calls and does not require that it is linked with an MPI library.
    git-svn-id: http://svn.code.sf.net/p/mpip/code/trunk@230 b8bbd010-f11e-0410-ac07-ecf92694ee1a

---
+++ diag_msgs_api.c
@@ -0,0 +1,78 @@
+/* -*- C -*- 
+
+   @PROLOGUE@
+
+   ----- 
+
+   Jeffrey Vetter vetter3@llnl.gov
+   Center for Applied Scientific Computing, LLNL
+   07 Feb 2001
+
+   diag.c -- diagnostic routines, error logging, warning, etc
+
+ */
+
+#ifndef lint
+static char *rcsid =
+  "$Header$";
+#endif
+
+#include <stdarg.h>
+
+#include "mpiPi.h"
+
+void
+mpiPi_msg (char *fmt, ...)
+{
+  va_list args;
+  FILE *fp = mpiPi.stdout_;
+  va_start (args, fmt);
+  fprintf (fp, "%s: ", mpiPi.toolname);
+  vfprintf (fp, fmt, args);
+  va_end (args);
+  fflush (fp);
+}
+
+void
+mpiPi_abort (char *fmt, ...)
+{
+  va_list args;
+  FILE *fp = mpiPi.stderr_;
+  va_start (args, fmt);
+  fprintf (fp, "\n\n%s: ABORTING: ", mpiPi.toolname);
+  vfprintf (fp, fmt, args);
+  va_end (args);
+  fflush (fp);
+  abort ();
+}
+
+void
+mpiPi_msg_debug (char *fmt, ...)
+{
+  va_list args; FILE *fp = mpiPi.stdout_;
+
+  if (mpiPi_debug <= 0)
+    return;
+
+  va_start (args, fmt);
+  fprintf (fp, "%s: DBG: ", mpiPi.toolname);
+  vfprintf (fp, fmt, args);
+  va_end (args);
+  fflush (fp);
+}
+
+void
+mpiPi_msg_warn (char *fmt, ...)
+{
+  va_list args;
+  FILE *fp = mpiPi.stderr_;
+  va_start (args, fmt);
+  fprintf (fp, "%s: WARNING: ", mpiPi.toolname);
+  vfprintf (fp, fmt, args);
+  va_end (args);
+  fflush (fp);
+}
+
+/* eof */
+
+
