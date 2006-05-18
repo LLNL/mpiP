452c6bab7de8876b4064be4ea131bff978590294
chcham <chcham@b8bbd010-f11e-0410-ac07-ecf92694ee1a>
2006-05-18 22:29:24 +0000

    Changes to reset callsite data with MPI_Pcontrol(3) call.  Can be used with MPI_Pcontrol(2) to generate periodic reports without accumulative data.

--- trunk/pcontrol.c
+++ trunk/pcontrol.c
@@ -17,8 +17,59 @@ static char *rcsid =
   "$Header$";
 #endif
 
+#include <float.h>
 #include "mpiPi.h"
 #include "symbols.h"
+
+void
+mpiPi_reset_callsite_data ()
+{
+  int ac, ndx;
+  callsite_stats_t **av;
+  callsite_stats_t *csp = NULL;
+
+  /* gather local task data */
+  h_gather_data (mpiPi.task_callsite_stats, &ac, (void ***) &av);
+
+  for (ndx = 0; ndx < ac; ndx++)
+    {
+      csp = av[ndx];
+
+      csp->maxDur = 0;
+      csp->minDur = DBL_MAX;
+      csp->maxIO = 0;
+      csp->minIO = DBL_MAX;
+      csp->maxDataSent = 0;
+      csp->minDataSent = DBL_MAX;
+
+      csp->count = 0;
+      csp->cumulativeTime = 0;
+      csp->cumulativeTimeSquared = 0;
+      csp->cumulativeDataSent = 0;
+      csp->cumulativeIO = 0;
+
+      csp->arbitraryMessageCount = 0;
+    }
+
+  if (time (&mpiPi.start_timeofday) == (time_t) - 1)
+    {
+      mpiPi_msg_warn ("Could not get time of day from time()\n");
+    }
+
+  mpiPi_GETTIME (&mpiPi.startTime);
+  mpiPi.cumulativeTime = 0;
+
+  mpiPi.global_app_time = 0;
+  mpiPi.global_mpi_time = 0;
+  mpiPi.global_mpi_size = 0;
+  mpiPi.global_mpi_io = 0;
+  mpiPi.global_mpi_msize_threshold_count = 0;
+  mpiPi.global_mpi_sent_count = 0;
+  mpiPi.global_time_callsite_count = 0;
+
+  free (av);
+}
+
 
 static int
 mpiPi_MPI_Pcontrol (const int flag)
@@ -43,6 +94,10 @@ mpiPi_MPI_Pcontrol (const int flag)
   else if (flag == 2)
     {
       mpiPi_generateReport ();
+    }
+  else if (flag == 3)
+    {
+      mpiPi_reset_callsite_data ();
     }
   else
     {
