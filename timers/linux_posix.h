3faa9562aa54f2b8c2f3dbb4332e949d57ecae9b
Chris Chambreau <cchambreau@users.noreply.github.com>
2007-04-13 21:53:07 +0000

    Remove clock_gettime and configure default and use gettimeofday instead. Changed clock_gettime clock to CLOCK_MONOTONIC.

--- timers/linux_posix.h
+++ timers/linux_posix.h
@@ -18,11 +18,11 @@
 /* read_real_time returns nanoseconds */
 #define mpiPi_TIMER double
 #define mpiPi_TIME  struct timespec
-#define mpiPi_TIMER_NAME    "clock_gettime"
+#define mpiPi_TIMER_NAME    "clock_gettime(CLOCK_MONOTONIC)"
 
 #define mpiPi_TIMER_INIT(timer_addr) {*(timer_addr) = 0;}
 #define mpiPi_ASNTIME(lhs,rhs) {bcopy(rhs, lhs, sizeof(mpiPi_TIME));}
-#define mpiPi_GETTIME(timeaddr) {clock_gettime(CLOCK_PROCESS_CPUTIME_ID, timeaddr);}
+#define mpiPi_GETTIME(timeaddr) {clock_gettime(CLOCK_MONOTONIC, timeaddr);}
 
 #define mpiPi_GETUSECS(timeaddr) ((((mpiPi_TIMER)((timeaddr)->tv_sec))*USECS) + ((mpiPi_TIMER)((timeaddr)->tv_nsec))/1000)
 #define mpiPi_GETTIMEDIFF(end,start) (mpiPi_GETUSECS(end)-mpiPi_GETUSECS(start))
